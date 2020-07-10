218501a538a7de02f938635d05a12b91e069665f
liyaozong <liyaozong@ghostcloud.cn>
2020-07-10 10:00:07 +0800

    first commit

---
+++ src/EffectiveLetterHeteroTopicWords.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 示例 1:
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ */
+public class EffectiveLetterHeteroTopicWords {
+    public static void main(String[] args) {
+        String s = "cat";
+        String t = "rac";
+        System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram2(s, t));
+
+    }
+    public static boolean isAnagram(String s, String t) {
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+
+        int[] count1 = new int[26];
+        for (int i=0;i<arrS.length;i++){
+            count1[arrS[i] - 'a']+=1;
+        }
+        int[] count2 = new int[26];
+        for (int i=0;i<arrT.length;i++){
+            count2[arrT[i] - 'a']+=1;
+        }
+
+        for (int i=0;i<count1.length;i++){
+            if (count1[i] != count2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+
+        return arrS.equals(arrT);
+    }
+}
---
+++ src/FirstUniqChar.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 387. 字符串中的第一个唯一字符
+ *
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ * 示例：
+ * s = "leetcode"
+ * 返回 0
+ * s = "loveleetcode"
+ * 返回 2
+ *
+ * 提示：你可以假定该字符串只包含小写字母。
+ */
+public class FirstUniqChar {
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        int result = FirstUniqChar(s);
+        System.out.println(result);
+    }
+
+    //    public static int FirstUniqChar(String s) {
+//        char[] chars = s.toCharArray();
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < chars.length; i++) {
+//            map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
+//        }
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            if (map.get(chars[i]) == 1) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    public static int FirstUniqChar(String s) {
+        // 因为只会出现小写字母，所以仅需26位
+        int[] count = new int[26];
+        char[] chars = s.toCharArray();
+
+        // 字母每出现一次就在对应下标位置的数组中加1
+        for(char c : chars){
+            count[c - 'a']+=1;
+        }
+
+        for(int i=0;i<chars.length;i++){
+            //表示只出现一次的字母
+            if(count[chars[i] - 'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
