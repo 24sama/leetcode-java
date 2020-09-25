7a50fec1a2061c457d9ba4cd235d9d0a51a1fac0
24sama <762474050@qq.com>
2020-09-25 15:30:33 +0800

    add CombinationSumIV

---
+++ src/array/CombinationSumIV.java
@@ -0,0 +1,75 @@
+package array;
+
+/**
+ * 组合总和 Ⅳ
+ * <p>
+ * 给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
+ * <p>
+ * 示例:
+ * nums = [1, 2, 3]
+ * target = 4
+ * <p>
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * <p>
+ * 请注意，顺序不同的序列被视作不同的组合。
+ * 因此输出为 7。
+ * <p>
+ * 进阶：
+ * 如果给定的数组中含有负数会怎么样？
+ * 问题会产生什么变化？
+ * 我们需要在题目中添加什么限制来允许负数的出现？
+ *
+ * @author liyaozong
+ * @date 2020/9/24 11:42
+ */
+public class CombinationSumIV {
+    public static void main(String[] args) {
+        System.out.println(combinationSum4(new int[]{1, 2, 3}, 4));
+        System.out.println(combinationSum4_2(new int[]{1, 2, 3}, 4));
+    }
+
+    /**
+     * 简单递归
+     */
+    public static int combinationSum4(int[] nums, int target) {
+        if (target == 0) {
+            return 1;
+        }
+        int res = 0;
+        for (int num : nums) {
+            if (target >= num) {
+                res += combinationSum4(nums, target - num);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * dp
+     * 在 0 这一点，我们定义 dp[0] = 1 的，它表示如果 nums 里有一个数恰好等于 target，它单独成为 1 种可能。
+     * dp[i]= dp[i - nums[0]] + dp[i - nums[1]] + dp[i - nums[2]] + ... （当 [] 里面的数 >= 0）
+     * dp[i] = sum{dp[i - num] for num in nums and if i >= num}
+     * 再举一个具体的例子：nums=[1, 3, 4], target=7;
+     * dp[7] = dp[6] + dp[4] + dp[3]
+     */
+    public static int combinationSum4_2(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num<=i) {
+                    dp[i] += dp[i-num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+}
